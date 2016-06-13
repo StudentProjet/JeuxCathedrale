b66750e6c68ac8ffd61682abbf78f674b8e9f193
kgz <kgzalexis@gmail.com>
2016-06-13 14:41:18 +0200

    Enigme3

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,4 +25,23 @@ $(document).ready(function() {
         }
     });
 
+    // Enigme 3
+    $("#btn-enigme3").bind('click', function () {
+        var reponse = $('input[name=optionsRadios2]:checked').val();
+        if(reponse != undefined){
+            if(reponse === "42m"){
+                var selector = "a[href=#" + reponse + "]";
+                $(selector).html(reponse);
+                $(selector).data("click", "true");
+                $('a[href=#enigme3]').hide();
+                $('#enigme1').modal("hide");
+                $("#btn-next").css('display', 'block');
+            }else{
+                $('#reponse_3').html("Faux ! essaie encore ");
+            }
+        }else{
+            $('#reponse_1').html("Repondez à la question");
+        }
+    });
+
 });
