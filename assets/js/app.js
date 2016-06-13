4efc4329fbf1e3ace1c710aae19447256db3ab54
MatthieuD_23 <delacroix.matthieu.23@gmail.com>
2016-06-13 15:12:31 +0200

    Enigme 4

--- assets/js/app.js
+++ assets/js/app.js
@@ -30,17 +30,36 @@ $(document).ready(function() {
         var reponse = $('input[name=optionsRadios2]:checked').val();
         if(reponse != undefined){
             if(reponse === "42m"){
-                var selector = "a[href=#" + reponse + "]";
-                $(selector).html(reponse);
+                var selector = "a[href=#1240]";
+                $(selector).html('1240');
                 $(selector).data("click", "true");
                 $('a[href=#enigme3]').hide();
-                $('#enigme1').modal("hide");
+                $('#enigme3').modal("hide");
                 $("#btn-next").css('display', 'block');
             }else{
                 $('#reponse_3').html("Faux ! essaie encore ");
             }
         }else{
-            $('#reponse_1').html("Repondez à la question");
+            $('#reponse_3').html("Repondez à la question");
+        }
+    });
+	
+	// Enigme 4
+    $("#btn-enigme4").bind('click', function () {
+        var reponse = $('input[name=textE4]').val();
+        if(reponse != ""){
+            if(reponse === "pierres de picquigny"){
+                var selector = "a[href=#1250]";
+                $(selector).html('1250');
+                $(selector).data("click", "true");
+                $('a[href=#enigme4]').hide();
+                $('#enigme4').modal("hide");
+                $("#btn-next").css('display', 'block');
+            }else{
+                $('#reponse_4').html("Faux ! essaie encore ");
+            }
+        }else{
+            $('#reponse_4').html("Repondez à la question");
         }
     });
 
