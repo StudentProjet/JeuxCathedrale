54a42cbceeba0d56970f7a2729c5785942b926a5
kperin <kristopher.perin@gmail.com>
2016-06-13 09:53:23 +0200

    enigme 1

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,23 @@ $(function(){
 
 $(document).ready(function() {
 
-    $("#btn-next").css('display', 'none');
+    // Enigme 1
+    $("#btn-enigme1").bind('click', function () {
+        var reponse = $('input[name=optionsRadios1]:checked').val();
+        if(reponse != undefined){
+            if(reponse === "1220"){
+                var selector = "a[href=#" + reponse + "]";
+                $(selector).html(reponse);
+                $(selector).data("click", "true");
+                $('a[href=#enigme1]').hide();
+                $('#enigme1').modal("hide");
+                $("#btn-next").css('display', 'block');
+            }else{
+                $('#reponse_1').html("Faux ! essaie encore ");
+            }
+        }else{
+            $('#reponse_1').html("Repondez à la question");
+        }
+    });
 
 });
